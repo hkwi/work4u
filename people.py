97b41eaaf59462416cf5818be17e0ed5c4c2e187
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2017-09-30 15:49:37 +0900

    remake wikidata query program

--- people.py
+++ people.py
@@ -1,4 +1,5 @@
 import rdflib
+import csv
 
 WD = rdflib.Namespace("http://www.wikidata.org/entity/")
 WDT = rdflib.Namespace("http://www.wikidata.org/wiki/Property:")
@@ -7,22 +8,23 @@ RDFS = rdflib.Namespace("http://www.w3.org/2000/01/rdf-schema#")
 # https://query.wikidata.org
 a = rdflib.ConjunctiveGraph(store="SPARQLStore")
 a.store.endpoint = "https://query.wikidata.org/bigdata/namespace/wdq/sparql"
-info = a.query('''
-SELECT ?s ?w ?name ?wname WHERE {
- ?s wdt:P39 ?w ; # 公職
-    wdt:P27 wd:Q17 ; # 国籍 日本
-    rdfs:label ?name .
- ?w rdfs:label ?wname .
- FILTER ( lang(?name)="ja" )
- FILTER ( lang(?wname)="ja" )
-} ORDER BY ?name
-''')
 
 b = rdflib.Graph()
 b.bind("wd", WD)
 b.bind("wdt", WDT)
-for s,w,name,wname in info:
-	b.add((s, WDT["P39"], w))
-	b.add((s, RDFS["label"], name))
-	b.add((w, RDFS["label"], wname))
+
+q = '''
+SELECT ?s ?name WHERE {
+ ?s rdfs:label ?name .
+} ORDER BY ?name
+'''
+for name, in csv.reader(open("docs/names.csv")):
+	r = a.query(q, initBindings={"name":rdflib.Literal(name, lang="ja")})
+	hit = False
+	for s,name in r:
+		b.add((s, RDFS["label"], name))
+		hit = True
+	if not hit:
+		print(name)
+
 open("docs/people.ttl","w").write(b.serialize(format="turtle").decode("UTF-8"))
