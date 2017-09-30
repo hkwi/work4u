8e8facb7d6b40c4827dd336c587bae4f42ab5c93
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2017-09-30 12:43:49 +0900

    add shugiin.go2senkyo.com extractor

---
+++ Makefile
@@ -0,0 +1,5 @@
+docs/senkyo_dotcom.csv:
+	python3 senkyo_dotcom.py
+
+docs/people.ttl:
+	python3 people.py
---
+++ senkyo_dotcom.py
@@ -0,0 +1,13 @@
+import lxml.etree
+import lxml.html
+import csv
+t=lxml.etree.parse("http://shugiin.go2senkyo.com/sitemap.xml")
+rows = []
+for url in t.xpath(".//s:loc/text()", namespaces={"s":"http://www.sitemaps.org/schemas/sitemap/0.9"}):
+	if not url.endswith("/"):
+		url += "/"
+	names = lxml.html.parse(url).xpath('.//p[@class="list_peason_name"]/text()')
+	rows += [(name,) for name in names]
+	print("%d from %s" % (len(names),url))
+
+csv.writer(open("senkyo_dotcom.csv", "w")).writerows((rows))
